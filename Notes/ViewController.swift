a96e5893fc5989a3998716f536e71cbf77cc7302
Edward White <edw@rdjam.es>
2020-07-22 10:54:13 -0400

    First / final commit

---
+++ Notes/ViewController.swift
@@ -0,0 +1,71 @@
+//
+//  ViewController.swift
+//  Notes
+//
+//  Created by Edward White on 7/19/20.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+    
+    var notes: [Note] = []
+    
+    @IBAction func createNote() {
+        let _ = NoteManager.main.create()
+        reload()
+    }
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        reload()
+    }
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return notes.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "NoteCell", for: indexPath)
+        cell.textLabel?.text = notes[indexPath.row].contents
+        return cell
+    }
+    
+    func reload() {
+        notes = NoteManager.main.getAllNotes()
+        self.tableView.reloadData()
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "NoteSegue" {
+            if let destination = segue.destination as?
+                NoteViewController {
+                destination.note = notes[tableView.indexPathForSelectedRow!.row]
+            }
+        }
+    }
+    
+    // this method handles row deletion
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        
+        if editingStyle == .delete {
+            
+            //Delete data from database
+            NoteManager.main.delete(note: notes[indexPath.row])
+
+            // remove the item from the data model
+            notes.remove(at: indexPath.row)
+
+            // delete the table view row
+            tableView.deleteRows(at: [indexPath], with: .fade)
+            
+        }
+        reload()
+    }
+}
+
